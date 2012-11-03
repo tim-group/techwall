9259e24bf409d80b2ccf2a4d52f0dee973afe8ad
scarytom <t.denley@cantab.net>
2012-11-03 10:43:08 +0000

    query and create technlolgies

--- src/techwall/technologies.clj
+++ src/techwall/technologies.clj
@@ -1,8 +1,20 @@
 (ns techwall.technologies
-  (:require [cheshire.core :as json]))
-
-(def ^{:private true} technologies [{:id 1 :name "java"} {:id 2 :name "scala"} {:id 3 :name "ruby"} {:id 4 :name "erlang"}])
+  (:use techwall.db)
+  (:require [cheshire.core :as json]
+            [clojureql.core :as ql]))
 
 (defn all [] 
-  {:headers {"Content-Type" "application/json"} :body (json/generate-string technologies)})
+  {:headers {"Content-Type" "application/json"} :body (json/generate-string @(ql/table :technologies))})
+
+(defn find-or-make
+  ([id name]
+    (if-let [actual-name @(ql/pick (ql/select (ql/table :technologies) (ql/where (= :id id))) :name)]
+      {:id id :name actual-name}
+      (find-or-make name)))
+  ([name]
+    (if-let [[id actual-name] @(ql/pick (ql/select (ql/table :technologies) (ql/where (= :upper/name (.toUpperCase name)))) [:id :name])]
+      {:id id :name actual-name}
+      (let [insert (ql/conj! (ql/table :technologies) {:name name})
+            id (:last-index (meta insert))]
+        {:id id :name name}))))
 
