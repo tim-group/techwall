bc445260e22b799ccc8251abbe862e62861853d5
scarytom <t.denley@cantab.net>
2012-11-04 11:32:00 +0000

    ween db off clojureql

--- src/techwall/db.clj
+++ src/techwall/db.clj
@@ -1,10 +1,8 @@
 (ns techwall.db
-  (:require [clojureql.core :as ql]
-            [clojure.java.jdbc :as jdbc]
+  (:require [clojure.java.jdbc :as jdbc]
             [techwall.schema :as schema]))
 
 (schema/create)
-(ql/open-global schema/db)
 
 (defn prepare-select [sql]
   (jdbc/prepare-statement schema/db sql {:result-type :forward-only :concurrency :read-only}))
@@ -24,10 +22,3 @@
       (into [] res))))
 
 (defn select-one [& queryandparams] (first (apply select queryandparams)))
-
-(if (= 0 (count @(ql/table :walls)))
-  (let [ins (ql/conj! (ql/table :walls) {:name "Example Wall"})]
-    (println (:last-index (meta ins)))))
-
-(if (= 0 (count @(ql/table :categories)))
-  (ql/conj! (ql/table :categories) [{:name "Radical"} {:name "Tentative"} {:name "Adopted"} {:name "Deprecated"} {:name "Obsolete"}]))
