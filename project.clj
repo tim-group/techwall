a0590d460845321fc0e6f83c441169bc550145e9
Tom Denley <tom.denley@youdevise.com>
2012-11-14 10:17:37 +0000

    version preparing for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techwall "0.1.0-SNAPSHOT"
+(defproject techwall "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
